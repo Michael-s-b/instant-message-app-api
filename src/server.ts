bd7a96ff981a548aafe9ccebfc2dc1815d9410c7
michael-s-b <michelsilva098@gmail.com>
2023-08-24 17:21:07 -0300

    -create prisma client and set User routes on express

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { PrismaClient } from "@prisma/client";
 import { app } from "./config";
+import Routes from "./routes";
+export const prismaClient = new PrismaClient();
 //setup routes
+app.use("/api", Routes.userRouter);
 
 //start server
 const PORT = process.env.PORT || 3000;
