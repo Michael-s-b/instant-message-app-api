5de88da02a5078fd90286218a09c05eef5c27250
michael-s-b <michelsilva098@gmail.com>
2023-08-24 17:12:15 -0300

    -add json and urlencoded parsing middleware

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,4 +1,6 @@
 import express from "express";
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 export default app;
