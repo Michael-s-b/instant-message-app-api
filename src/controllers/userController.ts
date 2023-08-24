3ff7a9584b936c9f3a7f1f91037936a8a7b67d45
michael-s-b <michelsilva098@gmail.com>
2023-08-24 17:22:19 -0300

    -implement userController methods getAllUsers and createUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from "express";
+import { User } from "../models";
 
 const userController = {
 	getAllUsers: async (req: Request, res: Response) => {
-		res.send("GET /users");
+		const allUsers = await User.findMany();
+
+		res.json({ data: allUsers }).status(200);
 	},
 	createUser: async (req: Request, res: Response) => {
-		res.send("POST /users");
+		const { username, email, password } = req.body;
+		try {
+			let existingUser = await User.findUnique({ where: { email } });
+			if (existingUser) {
+				res.json({ error: "User with given email already exists" }).status(400);
+				return;
+			}
+			existingUser = await User.findUnique({ where: { username } });
+			if (existingUser) {
+				res.json({ error: "User with given username already exists" }).status(400);
+				return;
+			}
+
+			const newUser = await User.create({ data: { username, email, passwordHash: password } });
+			res.json({ data: newUser }).status(201);
+		} catch (error) {
+			res.json({ error }).status(500);
+		}
 	},
 	getUserById: (req: Request, res: Response) => {
 		res.send("GET /users/:id");
