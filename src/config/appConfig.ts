31db29b09558797d6be9fec7c365ddd59127381a
michael-s-b <michelsilva098@gmail.com>
2023-08-26 07:54:44 -0300

    -add authTokenMiddleware globally

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { authTokenMiddleware } from "../middlewares";
 const app = express();
+// global middlewares
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(authTokenMiddleware); // for decoding jwt token and authenticating user
 
 export default app;
