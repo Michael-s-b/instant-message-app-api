354d6e6b2c7bc6b5e3ba12af326dd0e08b0ab62c
michael-s-b <michelsilva098@gmail.com>
2023-08-26 08:42:42 -0300

    -add Message model to prisma schema

---
+++ src/types/express.d.ts
@@ -0,0 +1,10 @@
+import { HttpError } from "http-errors";
+import { User } from "../models";
+declare global {
+	namespace Express {
+		interface Request {
+			userId: number | null;
+			authError: HttpError | null;
+		}
+	}
+}
