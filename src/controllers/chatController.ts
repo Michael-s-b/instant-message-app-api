4a42d78f00694ee2db66625b135eb103254c798c
michael-s-b <michelsilva098@gmail.com>
2023-08-27 18:01:16 -0300

    -implements service layer to Chat -move business logic from chatController to chatService

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -1,64 +1,22 @@
 import { Request, Response } from "express";
-import { Chat, User } from "../models";
-import { ChatType } from "../enums";
+import { chatService } from "../services";
 
 const chatController = {
 	getAllChats: async (req: Request, res: Response) => {
 		try {
-			const allChats = await Chat.findMany({
-				where: { users: { some: { userId: req.userId! } } },
-				include: { users: { select: { userId: true } } },
-			});
-			res.json({ data: allChats }).status(200);
+			const chatList = await chatService.getChatList(req.userId!);
+			res.json({ data: chatList }).status(200);
 		} catch (error: any) {
-			res.json({ error: error.message }).status(500);
+			return res.status(error.statusCode || 500).json({ error: error.message });
 		}
 	},
 	createDirectChat: async (req: Request, res: Response) => {
-		const { contact } = req.body;
-
+		const { contactId } = req.body;
 		try {
-			if (!contact) {
-				res.json({ error: "contact is required" }).status(401);
-				return;
-			}
-			const contactFound = await User.findFirst({
-				where: { OR: [{ username: contact }, { email: contact }] },
-			});
-			if (!contactFound) {
-				res.json({ error: "Contact not found" }).status(404);
-				return;
-			}
-			if (req.userId === contactFound.id) {
-				res.json({ error: "Cannot create chat with yourself" }).status(401);
-				return;
-			}
-			//check if a direct chat between these two users doesn't already exist
-			const existingChat = await Chat.findFirst({
-				where: {
-					AND: [
-						{ users: { some: { userId: req.userId! } } },
-						{ users: { some: { userId: contactFound.id } } },
-						{ type: ChatType[ChatType.DIRECT] },
-					],
-				},
-			});
-			if (existingChat) {
-				res.json({ error: "A direct chat between given users already exist" }).status(200);
-				return;
-			}
-
-			const newDirectChat = await Chat.create({
-				data: {
-					type: ChatType.DIRECT,
-					users: {
-						create: [{ userId: req.userId! }, { userId: contactFound.id }],
-					},
-				},
-			});
-			return res.json({ data: newDirectChat }).status(200);
+			const newChat = await chatService.createDirectChat(req.userId!, contactId);
+			res.json({ data: newChat }).status(201);
 		} catch (error: any) {
-			res.json({ error: error.message }).status(500);
+			return res.status(error.statusCode || 500).json({ error: error.message });
 		}
 	},
 	createGroupChat: async (req: Request, res: Response) => {
---
+++ src/services/chatService.ts
@@ -0,0 +1,58 @@
+import createError from "http-errors";
+import { ChatType } from "../enums";
+import { Chat, User } from "../models";
+const chatService = {
+	async getChatList(userId: any) {
+		try {
+			const chats = await Chat.findMany({
+				where: { users: { some: { userId: userId! } } },
+				include: { users: { select: { userId: true } }, messages: true },
+			});
+			return chats;
+		} catch (error: any) {
+			throw error;
+		}
+	},
+	async createDirectChat(userId: any, contactId: any) {
+		try {
+			if (!contactId) {
+				throw createError(401, "Contact is required");
+			}
+			const contactFound = await User.findFirst({
+				where: { OR: [{ username: contactId }, { email: contactId }] },
+			});
+			if (!contactFound) {
+				throw createError(404, "Contact not found");
+			}
+			if (userId === contactFound.id) {
+				throw createError(401, "Cannot create chat with yourself");
+			}
+			//check if a direct chat between these two users doesn't already exist
+			const existingChat = await Chat.findFirst({
+				where: {
+					AND: [
+						{ users: { some: { userId: userId } } },
+						{ users: { some: { userId: contactFound.id } } },
+						{ type: ChatType[ChatType.DIRECT] },
+					],
+				},
+			});
+			if (existingChat) {
+				throw createError(200, "A direct chat between given users already exist");
+			}
+
+			const newDirectChat = await Chat.create({
+				data: {
+					type: ChatType.DIRECT,
+					users: {
+						create: [{ userId: userId! }, { userId: contactFound.id }],
+					},
+				},
+			});
+			return newDirectChat;
+		} catch (error: any) {
+			throw createError(500, error.message);
+		}
+	},
+};
+export default chatService;
