e325a1d7882c8ac5df832af70488b96ee83cc65d
michael-s-b <michelsilva098@gmail.com>
2023-12-18 10:18:59 -0300

    -refactor userController and userRoute

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,19 +5,28 @@ import { HTTP_STATUS_CODE } from "../enums";
 
 class UserController {
 	//GET api/users
-	public async getAllUsers(req: Request, res: Response, next: NextFunction) {
+	public async getAllUsers(req: Request, res: Response, Next: NextFunction) {
 		let userService: UserService;
 		try {
 			userService = new UserServicePrisma();
 			const allUsers = await userService.getAllUsers();
-			res.json({ data: allUsers }).status(HTTP_STATUS_CODE.OK);
+			res.json(allUsers).status(HTTP_STATUS_CODE.OK);
 		} catch (error: any) {
-			next(error);
+			Next(error);
 		}
 	}
 	//GET api/users/:id
-	public async getUserById(req: Request, res: Response) {
-		res.send("GET /users/:id");
+	public async getUserById(req: Request, res: Response, Next: NextFunction) {
+		let userService: UserService;
+		const id = req.params.id;
+
+		try {
+			userService = new UserServicePrisma();
+			const user = await userService.getUserById({ id: parseInt(id) });
+			res.json(user).status(user ? HTTP_STATUS_CODE.OK : HTTP_STATUS_CODE.NOT_FOUND);
+		} catch (error: any) {
+			Next(error);
+		}
 	}
 	//PUT api/users/:id
 	public async updateUser(req: Request, res: Response) {
@@ -26,6 +35,19 @@ class UserController {
 	//DELETE api/users/:id
 	public async deleteUser(req: Request, res: Response) {
 		res.send("DELETE /users/:id");
+	}
+	//GET api/user
+	public async getMe(req: Request, res: Response, Next: NextFunction) {
+		let userService: UserService;
+		const id = req.userId;
+
+		try {
+			userService = new UserServicePrisma();
+			const user = await userService.getUserById({ id: id! });
+			res.json(user).status(user ? HTTP_STATUS_CODE.OK : HTTP_STATUS_CODE.NOT_FOUND);
+		} catch (error: any) {
+			Next(error);
+		}
 	}
 }
 export default UserController;
